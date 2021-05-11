4729d5c2df76c8f97f2bec9cfac75840e94e2e61
pacior00 <83274850+pacior00@users.noreply.github.com>
2021-05-11 13:52:33 +0200

    Update zadanie.py

--- zadanie.py
+++ zadanie.py
@@ -2,9 +2,13 @@ import geopandas
 import numpy as np
 import matplotlib.pyplot as plt
 
+# %%
+
 gdf = geopandas.read_file('shp/PD_STAT_GRID_CELL_2011.shp')
-gdf.to_crs("EPSG:4326")
+
 gdf['centroid'] = gdf.centroid
+
+# %%
 
 import shapely
 #
@@ -14,6 +18,8 @@ n_cells=30
 cell_size = (xmax-xmin)/n_cells
 #
 grid_cells = []
+
+# %%
 for x0 in np.arange(xmin, xmax+cell_size, cell_size ):
     for y0 in np.arange(ymin, ymax+cell_size, cell_size):
         # bounds
@@ -22,12 +28,18 @@ for x0 in np.arange(xmin, xmax+cell_size, cell_size ):
         grid_cells.append( shapely.geometry.box(x0, y0, x1, y1) )
 cell = geopandas.GeoDataFrame(grid_cells, columns=['geometry'])
 
+
+# %%
+
+
 ax = gdf.plot(markersize=.1, figsize=(12, 8), column='TOT', cmap='jet')
 
 plt.autoscale(False)
 cell.plot(ax=ax, facecolor="none", edgecolor='grey')
 ax.axis("off")
 
+# %%
+gdf = gdf.to_crs("EPSG:4326")
 merged = geopandas.sjoin(gdf, cell, how='left', op='within')
 dissolve = merged.dissolve(by="index_right", aggfunc="sum")
 cell.loc[dissolve.index, 'TOT'] = dissolve.TOT.values
