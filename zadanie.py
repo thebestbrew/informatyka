67d1b0634c70f8f6487d9cbe1824a3394593d27a
pacior00 <83274850+pacior00@users.noreply.github.com>
2021-05-14 12:34:49 +0200

    Update zadanie.py

--- zadanie.py
+++ zadanie.py
@@ -2,13 +2,15 @@ import geopandas
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %%
+
+
+#TO ODPALIC NAJPIERW
 
 gdf = geopandas.read_file('shp/PD_STAT_GRID_CELL_2011.shp')
 
+gdf = gdf.to_crs("EPSG:4326")
 gdf['centroid'] = gdf.centroid
-
-# %%
+#%%
 
 import shapely
 #
@@ -19,7 +21,6 @@ cell_size = (xmax-xmin)/n_cells
 #
 grid_cells = []
 
-# %%
 for x0 in np.arange(xmin, xmax+cell_size, cell_size ):
     for y0 in np.arange(ymin, ymax+cell_size, cell_size):
         # bounds
@@ -29,17 +30,13 @@ for x0 in np.arange(xmin, xmax+cell_size, cell_size ):
 cell = geopandas.GeoDataFrame(grid_cells, columns=['geometry'])
 
 
-# %%
-
-
 ax = gdf.plot(markersize=.1, figsize=(12, 8), column='TOT', cmap='jet')
 
 plt.autoscale(False)
 cell.plot(ax=ax, facecolor="none", edgecolor='grey')
 ax.axis("off")
 
-# %%
-gdf = gdf.to_crs("EPSG:4326")
+
 merged = geopandas.sjoin(gdf, cell, how='left', op='within')
 dissolve = merged.dissolve(by="index_right", aggfunc="sum")
 cell.loc[dissolve.index, 'TOT'] = dissolve.TOT.values
@@ -50,3 +47,80 @@ plt.axis('equal');
 plt.title('liczba ludności w siatce')
 
 #gdf.plot('TOT', legend = True)
+
+#%%
+
+
+
+gdf_w = geopandas.read_file('shp/Województwa.shp')
+gdf_w = gdf_w.to_crs('EPSG:4326')
+
+#
+cell = geopandas.GeoDataFrame(gdf_w, columns=['geometry'])
+
+
+ax = gdf.plot(markersize=.1, figsize=(12, 8), column='TOT_0_14', cmap='jet')
+
+plt.autoscale(False)
+cell.plot(ax=ax, facecolor="none", edgecolor='grey')
+ax.axis("off")
+
+
+merged = geopandas.sjoin(gdf, cell, how='left', op='within')
+dissolve = merged.dissolve(by="index_right", aggfunc="sum")
+cell.loc[dissolve.index, 'TOT_0_14'] = dissolve.TOT_0_14.values
+ax = cell.plot(column='TOT_0_14', figsize=(12, 8), cmap='viridis', vmax=700000, edgecolor="grey", legend = True)
+plt.autoscale(True)
+ax.set_axis_off()
+plt.axis('equal');
+plt.title('liczba ludności w województwach w wieku 0-14')
+
+
+#%%
+gdf_w = geopandas.read_file('shp/Województwa.shp')
+gdf_w = gdf_w.to_crs('EPSG:4326')
+
+#
+cell = geopandas.GeoDataFrame(gdf_w, columns=['geometry'])
+
+
+ax = gdf.plot(markersize=.1, figsize=(12, 8), column='TOT_15_64', cmap='jet')
+
+plt.autoscale(False)
+cell.plot(ax=ax, facecolor="none", edgecolor='grey')
+ax.axis("off")
+
+
+merged = geopandas.sjoin(gdf, cell, how='left', op='within')
+dissolve = merged.dissolve(by="index_right", aggfunc="sum")
+cell.loc[dissolve.index, 'TOT_15_64'] = dissolve.TOT_15_64.values
+ax = cell.plot(column='TOT_15_64', figsize=(12, 8), cmap='viridis', vmax=700000, edgecolor="grey", legend = True)
+plt.autoscale(True)
+ax.set_axis_off()
+plt.axis('equal');
+plt.title('liczba ludności w województwach w wieku 15-64')
+
+
+#%%
+gdf_w = geopandas.read_file('shp/Województwa.shp')
+gdf_w = gdf_w.to_crs('EPSG:4326')
+
+#
+cell = geopandas.GeoDataFrame(gdf_w, columns=['geometry'])
+
+
+ax = gdf.plot(markersize=.1, figsize=(12, 8), column='TOT_65__', cmap='jet')
+
+plt.autoscale(False)
+cell.plot(ax=ax, facecolor="none", edgecolor='grey')
+ax.axis("off")
+
+
+merged = geopandas.sjoin(gdf, cell, how='left', op='within')
+dissolve = merged.dissolve(by="index_right", aggfunc="sum")
+cell.loc[dissolve.index, 'TOT_65__'] = dissolve.TOT_65__.values
+ax = cell.plot(column='TOT_65__', figsize=(12, 8), cmap='viridis', vmax=700000, edgecolor="grey", legend = True)
+plt.autoscale(True)
+ax.set_axis_off()
+plt.axis('equal');
+plt.title('liczba ludności w województwach w wieku >65')
